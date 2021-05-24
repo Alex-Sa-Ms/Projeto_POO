a0dc0404562a913c7bfcad3504b532379365fb1f
Alex-Sa-Ms <a93315@alunos.uminho.pt>
2021-05-24 21:18:52 +0100

    small change

--- src/Player.java
+++ src/Player.java
@@ -5,13 +5,30 @@ public interface Player{
     //Unica Parte Comum ás duas classes
     Player Clone();
 
+
     //Parte necessária para reutilizar a classe 'Equipa'
+
+    /* Calcula a pontuação geral de um jogador */
+    int getOverall();
+
+    /*  De modo a manter o código reutilizável é necessário usar um método que seria tipico de C.
+        Calcula a pontuação geral de um jogador em função da posicao e a area de jogo.
+    *  Posicao:
+    *   - Guarda-Redes: 0
+    *   - Defesa:       1
+    *   - Medio:        2
+    *   - Avancado:     3
+    *  Area de Jogo:
+    *   - Esquerda:     0
+    *   - Centro:       1
+    *   - Direita:      2
+    * */
+    int getOverall(int pos, int area);
+
     int getShirtNumber();
 
     void setShirtNumber(int shirtNumber);
 
-    /* Calcula a pontuação geral de um jogador em função da sua subclasse */
-    int getOverall();
 
     //Parte necessária para reutilizar a classe 'Jogo'
 
